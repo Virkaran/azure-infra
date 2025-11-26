7d530708c99d03ddffd67028c78931c555a0d0a7
karan vir singh <virkaran41@gmail.com>
2025-11-26 23:29:11 +0530

    fourth commit

--- main.tf
+++ main.tf
@@ -24,15 +24,15 @@ resource "azurerm_key_vault" "kv" {
   tags = var.tags
 }
 
-resource "azurerm_key_vault_access_policy" "sp_policy" {
-  key_vault_id = azurerm_key_vault.kv.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  secret_permissions = [
-    "Get","List","Set","Delete","Recover","Backup","Restore","Purge"
-  ]
-}
+#resource "azurerm_key_vault_access_policy" "sp_policy" {
+#  key_vault_id = azurerm_key_vault.kv.id
+#  tenant_id    = data.azurerm_client_config.current.tenant_id
+#  object_id    = data.azurerm_client_config.current.object_id
+#
+#  secret_permissions = [
+#    "Get","List","Set","Delete","Recover","Backup","Restore","Purge"
+#  ]
+#}
 
 
 
