b342b7849903d0d641187a071349f3c739063d22
Virkaran <36572548+Virkaran@users.noreply.github.com>
2025-11-26 23:04:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,17 +63,6 @@ resource "azurerm_subnet" "aks_subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_cidr]
-  delegation {
-    name = "aks-delegation"
-    service_delegation {
-      name = "Microsoft.ContainerService/managedClusters"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action",
-        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
-        "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action"
-      ]
-    }
-  }
 }
 
 
